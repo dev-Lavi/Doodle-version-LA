1981db991e8e2f8fccd0b838613381719983c5c7
arjit203 <arjitgupta76@gmail.com>
2024-11-09 01:12:44 +0530

    Added Doodler Character

--- doodle jump/script.js
+++ doodle jump/script.js
@@ -6,7 +6,7 @@ let contex;
 
 
 //doodler or alien's character
-let doodlerWidth  = 46;
+let doodlerWidth = 46;
 let doodlerHeight = 46;
 let doodlerX = boardWidth/2 - doodlerWidth/2; //starting X-coordinate of the doodler, placing it at the center of the canvas horizontally
 let doodlerY = boardHeight*7/8 - doodlerHeight;  //initial Y-position of the doodler near the bottom of the canvas
@@ -28,5 +28,16 @@ window.onload = function() {//this function will work after loading of entire we
     board.width = boardWidth;
     context = board.getContext("2d"); // used for drawing on the board
 
+       //load doodler image
+       doodlerRightImg = new Image();
+       doodlerRightImg.src = "./doodler-right.png";
+       doodler.img = doodlerRightImg;
+       doodlerRightImg.onload = function() {
+       context.drawImage(doodler.img, doodler.x, doodler.y,doodler.width,doodler.height);
+       }
+   
+       doodlerLeftImg = new Image();
+       doodlerLeftImg.src = "./doodler-left.png";
+   
 }
 
